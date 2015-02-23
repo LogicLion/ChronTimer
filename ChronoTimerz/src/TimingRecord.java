b147da7acd83551063b7a223e9ad2ae66512abc3
Noah <Noah@Prometheus.gateway.pace.com>
2015-02-22 21:43:35 -0800

    Updated

--- ChronoTimerz/src/TimingRecord.java
+++ ChronoTimerz/src/TimingRecord.java
@@ -1,17 +1,54 @@
 import java.time.*;
 public class TimingRecord {
-private Instant start;
-private Instant finish;
-private Duration total;
-private enum status {
-	CANCEL, FINISH, DNF
+private Instant _start;
+private Instant _finish;
+private Duration _duration;
+private STATUS _status;
+private enum STATUS {
+	 CREATED, STARTED, DNF, FINISHED, CANCELLED
 }
 
 public TimingRecord()
 {
-start=Instant.MIN;
-finish=Instant.MAX;
-total=Duration.between(start, finish);
+_start=null;
+_finish=null;
+_duration=null;
+_status=STATUS.CREATED;
+}
+
+public void start(){
+	_start=Instant.now();
+	_status=STATUS.STARTED;
+}
+
+public void finish(){
+	_finish=Instant.now();
+	_status=STATUS.FINISHED;
+	_duration=Duration.between(_start,_finish);
+}
+
+public void DNF(){
+	_status=STATUS.DNF;
+}
+
+public void cancel(){
+	_status=STATUS.CANCELLED;
+}
+
+public Instant get_start() {
+	return _start;
+}
+
+public Instant get_finish() {
+	return _finish;
+}
+
+public Duration get_duration() {
+	return _duration;
+}
+
+public STATUS get_status() {
+	return _status;
 }
 
 }
