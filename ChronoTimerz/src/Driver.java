a120a6c05a02cdc3221272d9af1fece5980c67a2
Thomas Hocking <thomashocking7@gmail.com>
2015-03-02 00:59:42 -0600

    I think i fixed it...

--- ChronoTimerz/src/Driver.java
+++ ChronoTimerz/src/Driver.java
@@ -1,14 +1,28 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class Driver {
-public static void main(String[] args){
+public static void main(String[] args) throws Exception{
 	//Create command reader here.
 	
-	
+	ChronoTimer t = new ChronoTimer();
 	CommandReader r = new CommandReader();
 	Scanner input = new Scanner(System.in);
-	while(r.parse(input.nextLine()) != null);
+	//while(r.parse(input.nextLine()) != null);
+	while(true){
+		String line = input.nextLine();
+		String lineSplits[] = line.split(" ");
+		if(lineSplits[0].equals("TEST")){
+			Command.parseCommandFile(new File(lineSplits[1]));
+		}else{
+			Command c = Command.commandFromString(line);
+			c.execute(t);
+		}
+	}
+	
+	
 	
 	/*
 	IndividualStream test = new IndividualStream(10);
